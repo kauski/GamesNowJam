28153b8e80f8c4bccb5ed98a5926afc16562c24b
JaniKau2 <janikauhanen2@kamk.fi>
2024-02-22 01:36:23 +0200

    basic flying

---
+++ GamesNowJam/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+ 
+    
+
+
+    private float speedDecreaseRate = 3.0f;
+    private float minMoveSpeed = 15.0f;
+    private float turnSpeed = 80.0f; 
+    public float speedIncrease = 5f;
+    private float currentSpeed;
+    private float initialMoveSpeed = 5.0f;
+    private float liftForceMultiplier = 0.001f;
+    private float maxSpeed = 50.0f;
+   
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+    rb = GetComponent<Rigidbody>();
+        currentSpeed = initialMoveSpeed;
+    }
+
+    // Update is called once per frame
+   
+    private void Update()
+    {
+       //get inputs
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        bool speedUp = Input.GetKey(KeyCode.Space);
+        float verticalTurn = verticalInput * turnSpeed * Time.deltaTime;
+        float horizontalTurn = horizontalInput * turnSpeed * Time.deltaTime;
+        //rotate
+        transform.Rotate(verticalTurn, horizontalTurn, 0);
+
+        //adjust speed
+        if (speedUp && currentSpeed < maxSpeed)
+        {
+            currentSpeed += speedIncrease * Time.deltaTime;
+        }
+        else
+        {
+            currentSpeed = Mathf.Max(currentSpeed - speedDecreaseRate * Time.deltaTime, minMoveSpeed);
+        }
+        //apply up force and move forward
+        float liftForce = currentSpeed * liftForceMultiplier;
+        GetComponent<Rigidbody>().AddForce(Vector3.up * liftForce);
+        transform.Translate(Vector3.forward * currentSpeed * Time.deltaTime);
+
+    }
+ 
+
+}
