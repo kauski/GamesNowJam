8d4917655fb8a809918ab1f36d0ab7994718b784
Lv99-Thinkerbell <lv99.thinkerbell@gmail.com>
2024-02-25 06:51:58 +0800

    Added Gameplay Rules

--- GamesNowJam/Assets/Scripts/Test/Player.cs
+++ GamesNowJam/Assets/Scripts/Test/Player.cs
@@ -1,22 +1,34 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float baseMoveSpeed = 3f;
-    [SerializeField] private float maxBonusSpeed = 5f;
-    [SerializeField] private float decelerationTime = 1f;
-    [SerializeField] private float accelerationTime = 1f;
-    [SerializeField] private float baseFoV = 30f;
-    [SerializeField] private float maxFoV = 50f;
+    [Header("References")]
     [SerializeField] private TrailRenderer trail;
     [SerializeField] private Transform spawnPosition;
     [SerializeField] private Projectile poopPrefab;
+    [SerializeField] private Slider poopMeter;
+
+    [Header("Game Properties")]
+    [SerializeField] private float poopCost = 10f;
+    [SerializeField] private float poopRewardedPerTreat = 30f;
+    [SerializeField] private float maxPoop = 100f;
+
+    [Header("Movement Properties")]
+    [SerializeField] private float baseMoveSpeed = 3f;
+    [SerializeField] private float maxBonusSpeed = 5f;
+    [SerializeField] private float timeToReturnToOriginalSpeed = 1f;
+    [SerializeField] private float timeToReachMaxSpeed = 1f;
+    [SerializeField] private float defaultFoV = 30f;
+    [SerializeField] private float maxSpeedFoV = 50f;
+    private float currentPoopCount;
 
     private float moveSpeed;
 
     private void Start()
     {
         moveSpeed = baseMoveSpeed;
+        currentPoopCount = maxPoop;
     }
 
     void Update()
@@ -24,6 +36,7 @@ public class Player : MonoBehaviour
         AutoMove();
         AccelerateProcess();
         ShootProcess();
+        UpdateUI();
     }
 
     void AutoMove()
@@ -40,22 +53,22 @@ public class Player : MonoBehaviour
         trail.enabled = false;
         if (Input.GetKey(KeyCode.Space))
         { 
-            moveSpeed += accelerationTime * Time.deltaTime;
+            moveSpeed += timeToReachMaxSpeed * Time.deltaTime;
             trail.enabled = true;
         }
         else if (moveSpeed > baseMoveSpeed)
-            moveSpeed -= decelerationTime * Time.deltaTime;
+            moveSpeed -= timeToReturnToOriginalSpeed * Time.deltaTime;
         moveSpeed = Mathf.Clamp(moveSpeed, baseMoveSpeed, baseMoveSpeed + maxBonusSpeed);
 
         float lerpFactor = (moveSpeed - baseMoveSpeed) / maxBonusSpeed;
 
         //Hardcode hehe whatevs
-        Camera.main.fieldOfView = Mathf.Lerp(baseFoV, maxFoV, lerpFactor);
+        Camera.main.fieldOfView = Mathf.Lerp(defaultFoV, maxSpeedFoV, lerpFactor);
     }
 
     void ShootProcess()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && currentPoopCount >= poopCost)
         {
             // Use the current transform's forward direction as the new object's forward direction
             Quaternion spawnRotation = Quaternion.LookRotation(Camera.main.transform.forward);
@@ -63,6 +76,24 @@ public class Player : MonoBehaviour
             // Instantiate the prefab at the calculated position and rotation
             Projectile proj = Instantiate(poopPrefab, spawnPosition.position, spawnRotation);
             proj.Set();
+
+            currentPoopCount -= poopCost;
+        }
+    }
+
+    void UpdateUI()
+    {
+        poopMeter.value = currentPoopCount / maxPoop;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Treats")
+            currentPoopCount = Mathf.Clamp(currentPoopCount + poopRewardedPerTreat, 0f, maxPoop);
+        else if (other.gameObject.tag == "Enemy")
+        {
+            //Player died
+            Debug.Log("Player lost - Do Something");
         }
     }
 }
